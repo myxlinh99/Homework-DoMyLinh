caf3c135e46b78aea97863823c009f15394968e2
LAPTOP-QP4LB6MT\Allie <allielinhdo@gmail.com>
2017-07-27 22:23:26 +0700

    Session 2 homework

--- Session 2/stars.py
+++ Session 2/stars.py
@@ -1,8 +1,21 @@
 for i in range (40):
     print("*",end="")
 print()
+
+
 for j in range(40):
     print("x*",end="")
 print()
 
 
+
+for k in range(10):
+    if k%2==0:
+        print()
+        for a in range(5):
+            print("*x",end="")
+        print()
+    else:
+        for b in range(5):
+            print("x*",end="")
+            
