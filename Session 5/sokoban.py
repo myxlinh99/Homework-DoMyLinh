1600b4e177a1daf1a56128643a61fe6667ae9e32
LAPTOP-QP4LB6MT\Allie <allielinhdo@gmail.com>
2017-08-06 23:10:38 +0700

    Session 5 Homework

---
+++ Session 5/sokoban.py
@@ -0,0 +1,41 @@
+px = 2
+py = 1
+bx = 1
+by = 2
+gx = 1
+gy = 3
+while True:
+    for y in range(4):
+        for x in range (4):
+            if x == px and y == py:
+                print("P",end=" ")
+            elif x == bx and y == by:
+                print("B",end =" ")
+            elif x == gx and y == gy:
+                print (".", end = " ")
+            else:
+                print("-", end= " ")
+            
+        print() #newline
+    command = input("Your move? ")
+    if command.upper()== "D":
+        px +=1
+    if command.upper()=="A":
+        px -=1
+    if command.upper()=="S":
+        py += 1
+    if command.upper()=="W":
+        py -= 1
+    if command.upper() == "D" and px== bx and py ==by:
+        bx += 1
+    if command.upper() =="S" and px == bx and py ==by:
+        by +=1
+    if command.upper() == "A" and px == bx and py == by:
+        bx -= 1
+    if command.upper() == "W" and px == bx and py == by:
+        by -= 1
+    if bx == gx and by == gy:
+        print ("You won! Congrats!!")
+
+    
+        
