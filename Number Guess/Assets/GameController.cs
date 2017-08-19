8de9a30c4c5e19aa15ebc0b415268e44bfdd4c2c
LAPTOP-QP4LB6MT\Allie <allielinhdo@gmail.com>
2017-08-19 12:36:25 +0700

    Homework

---
+++ Number Guess/Assets/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour {
+	public Text DisplayText;
+	public Button YesButton;
+	public Button LowerButton;
+	public Button HigherButton;
+	int min = 0;
+	int max = 100;
+	int result;
+
+	// Use this for initialization
+	void Start () {
+		Yes ();
+	}
+	public void Result ()
+	{
+		result = (min + max + 1) / 2;
+	}
+	public void Yes()
+	{
+		DisplayText.text = "[NEW GAME] Pick a number between 0 and 100 but don't tell me you Puny Human. Is your number 50?";
+
+	}
+	public void Lower()
+	{
+		max = result;
+		int NewResult = (min+max+1)/2;
+		DisplayText.text = "Is your number " + NewResult + "?";
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
